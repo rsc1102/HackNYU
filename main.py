172c621399d05426de11149f063db5a11107f9c6
rsc1102 <rohitchaudhari1102@gmail.com>
2025-02-08 20:51:55 -0500

    promo handling

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import streamlit as st
 import requests
 import pandas as pd
 import altair as alt
+import json
+import random
+import string
+
+def generate_random_alphanumeric_string(length):
+  """Generates a random alphanumeric string of specified length."""
+  characters = string.ascii_letters + string.digits
+  return ''.join(random.choice(characters) for i in range(length))
+
 
 st.set_page_config(
         page_title="Smart Spend", 
@@ -62,19 +71,45 @@ with st.form("my_form"):
             latest_month_transactions = df[df['Month'] == df['Month'].max()]
             latest_moth_spend = latest_month_transactions.groupby(latest_month_transactions['category'])['amount'].sum().reset_index()
                   
-        
-  
+
+
 if submitted:      
     with st.container(border=True):
-        col1,col2 = st.columns([0.3,0.7],border=True)
+        st.title("Promos")
+        
+        streak = 0
+        for i in range(len(monthly_spend)-1,0,-1):
+            if monthly_spend['Total Spend'][i] > monthly_spend['Total Spend'][i-1]:
+                break
+            streak += 1
+        
+        with open('Promos.json', 'r') as file:
+            dictionary_list = json.load(file)
+        
+        cols = st.columns(6,border=True)
+        
+        for i,item in enumerate(random.sample(dictionary_list,6)):
+            applied_streak = min(item['Max Streak'],streak)
+            if applied_streak != 0:
+                promo = {
+                    "Partner":item['Partner'],
+                    "Item":item['Item Name'],
+                    "Price":item['Price'],
+                    "Discount Applied":f"""{item[f"Streak {applied_streak} Discount"]}%""",
+                    "New Price": item['Price'] - (item['Price']*item[f"Streak {applied_streak} Discount"]/100),
+                    "Promo Code": generate_random_alphanumeric_string(6)
+                }
+                with cols[i]:
+                    st.write(promo)
+            
+            
+    with st.container(border=True):
+        st.title("Your Spending Habits:")      
+        col1, col2, col3 = st.columns([0.2,0.4,0.4],border=True)
         
         with col1:
             st.title("Streak 🔥")
-            streak = 0
-            for i in range(len(monthly_spend)-1,0,-1):
-                if monthly_spend['Total Spend'][i] > monthly_spend['Total Spend'][i-1]:
-                    break
-                streak += 1
+            
                 
             st.html(
                 f"""
@@ -83,21 +118,12 @@ if submitted:
                 </div>
                 """
             )
-            
-            
-        with col2:
-            st.title("Promos")
-            
-            
-    with st.container(border=True):
-        st.title("Your Spending Habits:")      
-        col1, col2 = st.columns(2,border=True)
 
-        with col1:
+        with col2:
             st.header("Monthly Trend")
             st.bar_chart(monthly_spend,x="Month",y="Total Spend")
 
-        with col2:
+        with col3:
             st.header("Spend Breakdown")
             chart = alt.Chart(latest_moth_spend).mark_arc().encode(
             theta="amount",
